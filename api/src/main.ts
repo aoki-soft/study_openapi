b62ebd74a1ff6ac7fcdf0ddc8b4d59f9c55b74ef
aoki-soft <aoki.soft@outlook.jp>
2022-08-07 17:08:51 +0900

    add openapi-expressを追加した

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,7 @@
 import {getLogger} from "log4js";
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
+import { initialize } from "express-openapi";
+import path from "path";
 
 let logger = getLogger();
 logger.level = process.env.LOG_LEVEL || 'debug';
@@ -9,12 +11,24 @@ logger.info("Hello World!!");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const port = process.env.PORT || 3000;
+
+initialize({
+	app: app,
+	apiDoc: "/openapi/openapi.yml",
+	validateApiDoc: true,
+	operations: {
+		getUser: [
+			function (req: Request, res: Response) {
+				res.send({
+          id: 1,
+					name: "hatano"
+				});
+			}
+		]
+	}
+});
+
+const port = process.env.PORT || 4050;
 app.listen(port, () => {
 	logger.info(`Start on port ${port}`);
-});
-app.get("/user", (req: Request, res: Response) => {
-	res.send({
-		name: "hatano"
-	});
 });
