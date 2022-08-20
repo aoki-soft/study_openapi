6366dc73c2c8fed3a78e7a2ce70bb69d5ee4157c
aoki-soft <aoki.soft@outlook.jp>
2022-08-21 03:48:10 +0900

    apiサーバ基本部分コード追加

---
+++ api/src/logger.ts
@@ -0,0 +1,61 @@
+import { getLogger, configure, addLayout, LoggingEvent } from "log4js";
+import { install } from "source-map-support";
+
+export function initLogger() {
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  install({
+    environment: 'node'
+  });
+
+
+  const logLevel: string = process.env.LOG_LEVEL ||'all';
+
+  addLayout("json", function() {
+    return function(logEvent: LoggingEvent) {
+      let source = undefined;
+      if (logEvent.fileName && logEvent.lineNumber) {
+        source = `${logEvent.fileName}:${logEvent.lineNumber}`;
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const data = logEvent.data[0];
+      const logEventJson = {
+        time: logEvent.startTime,
+        level: logEvent.level.levelStr,
+        category: logEvent.categoryName,
+        source: source,
+        function: logEvent.functionName,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        context: logEvent.context,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        data: data
+      }
+      return JSON.stringify(logEventJson);
+    };
+  });
+
+  const config = {
+    appenders: {
+      out: { type: "stdout", layout: { type: "json" } },
+    },
+    categories: {
+      default: { appenders: ["out"], level: logLevel, enableCallStack: true },
+    },
+  };
+  configure(config);
+
+  const logger = getLogger();
+
+  process.on('uncaughtException', function(error) {
+    logger.fatal({
+      message: "uncaughtException",
+      error: error
+    });
+    process.exit(1);
+  }); 
+
+  logger.info({
+    message: "ロガーセットアップ完了",
+    loggerConfig: config
+  })
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,34 +1,105 @@
-import {getLogger} from "log4js";
-import express, { Request, Response, NextFunction } from "express";
+import {getLogger, Logger} from "log4js";
+import express, { Request, Response } from "express";
 import { initialize } from "express-openapi";
-import path from "path";
-
-let logger = getLogger();
-logger.level = process.env.LOG_LEVEL || 'debug';
-logger.info("Hello World!!");
+import { initLogger } from "./logger";
+import { exit } from "process";
 
 
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+void (async ()=>{
+	initLogger();
+	const logger = getLogger();
+	logger.info({
+		message: "apiサーバ起動開始"
+	});
 
-initialize({
-	app: app,
-	apiDoc: "/openapi/openapi.yml",
-	validateApiDoc: true,
-	operations: {
-		getUser: [
-			function (req: Request, res: Response) {
-				res.send({
-          id: 1,
-					name: "hatano"
-				});
-			}
-		]
+	const app = express();
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
+	app.use((req, res, next) => {
+		const logger = getLogger();
+		const accessId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+		logger.addContext("accessId", accessId);
+		logger.trace({
+			message: "アクセスログ",
+			path: req.url
+		})
+		res.locals.logger = logger;
+		next();
+	})
+	
+	await initialize({
+		app: app,
+		apiDoc: "/openapi/openapi.yml",
+		validateApiDoc: true,
+		operations: {
+			getUser: [
+				function (req: Request, res: Response) {
+					const logger = res.locals.logger as Logger;
+					logger.trace({
+						message: "get /users",
+						...req.body});
+					const resBody = JSON.stringify({
+						id: 1,
+						name: "hatano"
+					});
+					logger.trace({resBody: resBody});
+					res.send(resBody);
+					logger.trace({message: "レスポンス成功"});
+				}
+			]
+		}
+	});
+
+	const port = process.env.PORT || 4050;
+	const httpServer = app.listen(port, () => {
+		logger.debug({
+			message: "HTTPサーバ起動中",
+			port: port
+		});
+	});
+
+	httpServer.addListener("error", (error)=>{
+		logger.error({
+			message: "HTTPサーバ起動失敗",
+			error: error
+		})
+		exit(1)
+	})
+
+	function okListeng() {
+		logger.info({
+			message: "HTTPサーバ起動成功",
+			port: port
+		})
 	}
-});
+	httpServer.addListener("listening", okListeng)
 
-const port = process.env.PORT || 4050;
-app.listen(port, () => {
-	logger.info(`Start on port ${port}`);
-});
+	function closeHttpServer() {
+		logger.fatal({
+			message: "HTTPサーバ終了しました",
+			port: port
+		})
+		exit(1)
+	}
+	httpServer.addListener("close", closeHttpServer)
+
+	process.on('SIGTERM', () => {
+		logger.info({
+			message: "SIGTERMを受け取りました"
+		})
+		httpServer.removeListener("close", closeHttpServer);
+		function okCloseHttpServer() {
+			logger.info({
+				message: "HTTPサーバを正常に終了しました"
+			});
+			exit(0);
+		}
+		httpServer.addListener("close", okCloseHttpServer);
+		httpServer.close(() => {
+			logger.info({
+				message: "HTTPサーバを終了します"
+			})
+		})
+	});
+	
+})();
